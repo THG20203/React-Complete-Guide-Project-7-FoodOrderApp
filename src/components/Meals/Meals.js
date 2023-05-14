6d1f2507ce2f0efa6ff0d99aef05a7149b40e1a6
THG20203 <tristanhgriffiths8@yahoo.com>
2023-05-14 22:40:50 +0100

    adding meals component to app

--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,10 +1,12 @@
 import { Fragment } from "react";
 
+/* importing other two other meals components */
 import MealsSummary from "./MealsSummary";
 import AvailableMeals from "./AvailableMeals";
 
 const Meals = () => {
   return (
+    /* returning two elements side by side -> one root JSX element so Fragment. */
     <Fragment>
       <MealsSummary />
       <AvailableMeals />
