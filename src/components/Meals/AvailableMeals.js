1a1ce82caaee8064c6faf1f46aae6e35729ab202
THG20203 <tristanhgriffiths8@yahoo.com>
2023-05-15 23:51:31 +0100

    debugging

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,6 +1,6 @@
-import classes from "./AvailableMeals.module.css";
-import MealItem from "./MealItem/MealItem";
 import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+import classes from "./AvailableMeals.module.css";
 
 const DUMMY_MEALS = [
   {
@@ -38,6 +38,7 @@ const AvailableMeals = () => {
   meal.price. */
   const mealsList = DUMMY_MEALS.map((meal) => (
     <MealItem
+      id={meal.id}
       key={meal.id}
       name={meal.name}
       description={meal.description}
