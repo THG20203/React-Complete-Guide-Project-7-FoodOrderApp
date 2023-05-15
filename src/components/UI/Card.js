cbc72566ac2758a3f198cd92c4008287a1fb83bc
THG20203 <tristanhgriffiths8@yahoo.com>
2023-05-15 20:57:38 +0100

    card.js less static code

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,7 +1,11 @@
 import classes from "./Card.module.css";
 
 const Card = (props) => {
-  return <div>Card</div>;
+  return (
+    /* props children -> so whatever is passed between the opening and closing tag of the card
+  component is in the end used inside of the Card. */
+    <div className={classes.card}>{props.children}</div>
+  );
 };
 
 export default Card;
