4a79dde1ed0e4ebec1fdf685669400e0a768055d
THG20203 <tristanhgriffiths8@yahoo.com>
2023-05-19 00:02:28 +0100

    beginning modal work

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,37 @@
+import { Fragment } from "react";
+import ReactDOM from "react-dom";
+
+import classes from "./Modal.module.css";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById("overlays");
+
+/* Special kind of markup -> want to use react portal for both my backdrop (behind modal overlay
+which blocks interaction with the rest of page) and I also want to render the modal overlay itself 
+with react portal. */
+/* This is to use the component wherever I want, but render the actual HTML elements in a specific 
+place in the DOM tree */
+const Modal = (props) => {
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </Fragment>
+  );
+};
+
+export default Modal;
